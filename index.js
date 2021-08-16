8393ef38400d7330708b6b4c28aed82e0c4bcf9f
ouiam-dot <ouiamsui27@gmail.com>
2021-08-16 13:35:20 +0100

    port number isn't required in 3.0+ versions

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/productsdb",{
+mongoose.connect('mongodb://localhost/productsdb',{
     useNewUrlParser:true,
      useUnifiedTopology:true}
      );
