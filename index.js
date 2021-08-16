729621e096c7f798ea73a9c9621aed948b3d91f5
ouiam-dot <ouiamsui27@gmail.com>
2021-08-16 13:22:56 +0100

    edit connection to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 
 const app = express();
 const PORT = 4000;
@@ -15,3 +17,8 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/productsdb",{
+    useNewUrlParser=true,
+     useUnifiedTopology=true}
+     );
