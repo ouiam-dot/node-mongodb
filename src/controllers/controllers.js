8501e3a766e6c4f9dadd78f6f2780f46ce11540c
ouiam-dot <ouiamsui27@gmail.com>
2021-08-16 16:46:09 +0100

    add delete and update functions

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -15,7 +15,7 @@ export const addnewProduct = (req, res) => {
     });
 }
 
-export const getProducts = (res) => {
+export const getProducts = (req, res) => {
     Product.find({}, (err, Product) => {
         if (err) {
             res.send(err);
@@ -30,5 +30,23 @@ export const getProductWithID = (req, res) => {
             res.send(err);
         }
         res.json(Product);
+    });
+}
+
+export const updateProduct = (req, res) => {
+    Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false }, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Product);
+    });
+}
+
+export const deleteProduct = (req, res) => {
+    Product.deleteOne({ _id: req.params.ProductID }, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'successfully deleted product' });
     });
 }
