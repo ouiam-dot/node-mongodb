4c698cfbc6a82254b1dc1563e186cc7cde711000
ouiam-dot <ouiamsui27@gmail.com>
2021-08-16 13:28:16 +0100

    typo

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.listen(PORT, () =>
 );
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/productsdb",{
-    useNewUrlParser=true,
-     useUnifiedTopology=true}
+    useNewUrlParser:true,
+     useUnifiedTopology:true}
      );
